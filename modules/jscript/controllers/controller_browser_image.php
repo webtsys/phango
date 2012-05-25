2f65e678a4284d57985e1b316e09821968c255bd
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-05-25 17:02:38 +0200

    Finish browser for load images in ckeditor

--- modules/jscript/controllers/controller_browser_image.php
+++ modules/jscript/controllers/controller_browser_image.php
@@ -7,51 +7,50 @@ function Browser_image()
 	
 	load_lang('jscript');
 	
-	load_libraries(array('check_admin', 'send_email'));
+	load_model('jscript');
+	
+	load_libraries(array('check_admin', 'send_email', 'generate_admin_ng'));
 	
 	if(check_admin($user_data['IdUser']))
 	{
-	
-		//$_POST
-		//settype($_FILE, 'string');
-		//print_r($_FILE);
-		/*ob_start();
-		
-		print_r($_FILES);
-		
-		$content=ob_get_contents();
-		
-		ob_end_clean();
-		
-		send_mail('webmaster@web-t-sys.com', 'cosas', $content, $content_type='plain', $bcc='');
-		
-		echo 'pepe';
-		Array
-(
-    [upload] => Array
-        (
-            [name] => amigos.jpg
-            [type] => image/jpeg
-            [tmp_name] => /tmp/phphpBb2U
-            [error] => 0
-            [size] => 42964
-        )
-
-)
-		*/
 		
 		ob_start();
 		
+		//Obtain headers..
+		//http://localhost/phangodev/index.php/jscript/show/browser_image/browser_image/?CKEditor=text[es-ES]&CKEditorFuncNum=2&langCode=es
 		
+		settype($_GET['CKEditorFuncNum'], 'integer');
+		
+		?>
+		<script type="text/javascript" src="<?php echo make_fancy_url($base_url, 'jscript', 'load_jscript', 'script', array('no_compression' => 1, 'input_script' => 'ckeditor_path.js.php')); ?>"></script>
+		<script type="text/javascript" src="<?php echo make_fancy_url($base_url, 'jscript', 'load_jscript', 'script', array('no_compression' => 0, 'input_script' => 'textbb--ckeditor--ckeditor.js') ); ?>"></script>
+		<script language="Javascript">
+		
+		function put_url(num_editor, url)
+		{
+		
+			window.opener.CKEDITOR.tools.callFunction( num_editor, url); 
+			window.close();
+		
+		}
+		
+		</script>
+		<?php
+		
+		$headers=ob_get_contents();
+		
+		ob_clean();
+		
+		$arr_fields=array('image');
+		
+		$url_options='';
+		
+		ListModel('jscript_image', $arr_fields, $url_options, $options_func='ImageOptionsListModel', $where_sql='', $arr_fields_form=array(), $type_list='Basic');
 		
 		$content=ob_get_contents();
 		
 		ob_end_clean();
-	
-		$path=$base_path.'application/media/upload_images/';
-		$url_path=$base_url.'/media/upload_images/';
 		
-		$headers='';
 		$title=$lang['jscript']['search_images'];
 		
 		echo load_view(array($title, $content, $block_title=array(), $block_content=array(), $block_urls=array(), $block_type=array(), $block_id=array(), $config_data, $headers), 'admin/admin_none');
@@ -59,5 +58,23 @@ function Browser_image()
 	}
 
 }
+
+function ImageOptionsListModel($url_options, $model_name, $id, $arr_row)
+{
+
+	global $lang, $model;
+
+	$arr_options=BasicOptionsListModel($url_options, $model_name, $id);
+	
+	$url_image=$model['jscript_image']->components['image']->show_image_url($arr_row['image']);
+	
+	$func_js='put_url(\''.$_GET['CKEditorFuncNum'].'\', \''.$url_image.'\');';
+	
+	$arr_options[]='<a href="javascript:'.$func_js.'">'.$lang['jscript']['add_image_to_form'].'</a>';
+	
+	return $arr_options;
+
+}
+
 
 ?>
--- modules/jscript/controllers/controller_upload_image.php
+++ modules/jscript/controllers/controller_upload_image.php
@@ -14,40 +14,8 @@ function Upload_image()
 	if(check_admin($user_data['IdUser']))
 	{
 	
-		//$_POST
-		//settype($_FILE, 'string');
-		//print_r($_FILE);
-		/*ob_start();
-		
-		print_r($_FILES);
-		
-		$content=ob_get_contents();
-		
-		ob_end_clean();
-		
-		send_mail('webmaster@web-t-sys.com', 'cosas', $content, $content_type='plain', $bcc='');
-		
-		echo 'pepe';
-		Array
-(
-    [upload] => Array
-        (
-            [name] => amigos.jpg
-            [type] => image/jpeg
-            [tmp_name] => /tmp/phphpBb2U
-            [error] => 0
-            [size] => 42964
-        )
-
-)
-		*/
-		
 		settype($_FILES['name'], 'string');
 		
-		$path=$base_path.'application/media/upload_images/';
-		$url_path=$base_url.'/media/upload_images/';
-		
-		//$image_field=new ImageField('upload', $path, $url_path);
 		
 		if( $model['jscript_image']->insert(array(  'image' =>  'upload')) )
 		{
