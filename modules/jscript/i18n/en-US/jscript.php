676c649a0859f5e8c268f8050b3f3904c69e8523
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-05-25 17:18:11 +0200

    Fixes

---
+++ modules/jscript/i18n/en-US/jscript.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['jscript']['image_uploaded_successfully']='image_uploaded_successfully';
+
+$lang['jscript']['image_no_uploaded_successfully']='image_no_uploaded_successfully';
+
+$lang['jscript']['search_images']='search_images';
+
+$lang['jscript']['add_image_to_form']='add_image_to_form';
+
+?>
