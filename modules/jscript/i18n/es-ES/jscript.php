e1164fec2947b45fe1cc0c173b53748704a74331
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-05-25 17:30:28 +0200

    Added modifications to jscript i18n

--- modules/jscript/i18n/es-ES/jscript.php
+++ modules/jscript/i18n/es-ES/jscript.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['jscript']['image_uploaded_successfully']='image_uploaded_successfully';
+$lang['jscript']['image_uploaded_successfully']='Image subida correctamente';
 
-$lang['jscript']['image_no_uploaded_successfully']='image_no_uploaded_successfully';
+$lang['jscript']['image_no_uploaded_successfully']='Imagen no subida correctamente, por favor asegúrese que tiene permisos para subir la imagen';
 
-$lang['jscript']['search_images']='search_images';
+$lang['jscript']['search_images']='Buscar imágenes';
 
-$lang['jscript']['add_image_to_form']='add_image_to_form';
+$lang['jscript']['add_image_to_form']='Añadir imagen al formulario;
 
 ?>
