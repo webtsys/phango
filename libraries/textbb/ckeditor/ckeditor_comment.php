762f952a281e4d6bd73093a41b45211ff4f8e399
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-05-28 03:40:21 +0200

    Added contents to INSTALL

--- libraries/textbb/ckeditor/ckeditor_comment.php
+++ libraries/textbb/ckeditor/ckeditor_comment.php
@@ -57,7 +57,7 @@ function load_jscript_editor($name_editor, $value, $profiles='all')
 				echo '\''.implode('\',\'', $smiley_img).'\'';
 
 				?>
-				//'regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','tounge_smile.gif','embaressed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angel_smile.gif','shades_smile.gif', 'cry_smile.gif','kiss.gif'
+				
 			],
 			
 			smiley_descriptions :
@@ -80,8 +80,7 @@ function load_jscript_editor($name_editor, $value, $profiles='all')
 
 				?>
 				
-				//'smiley', 'sad', 'wink', 'laugh', 'cheeky', 'blush', 'surprise',
-				//'indecision', 'angel', 'cool', 'crying'
+
 			]
 
 			,on :
@@ -143,40 +142,6 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 			infoTab.remove( 'browse' );
 			dialogDefinition.removeContents( 'target' );
 			dialogDefinition.removeContents( 'advanced' );
-			/*
-			// Set the default value for the URL field.
-			var urlField = infoTab.get( 'url' );
-			urlField['default'] = 'www.example.com';
-
-			// Remove the "Target" tab from the "Link" dialog.
-
-			// Add a new tab to the "Link" dialog.
-			dialogDefinition.addContents({
-				id : 'customTab',
-				label : 'My Tab',
-				accessKey : 'M',
-				elements : [
-					{
-						id : 'myField1',
-						type : 'text',
-						label : 'My Text Field'
-					},
-					{
-						id : 'myField2',
-						type : 'text',
-						label : 'Another Text Field'
-					}
-				]
-			});
-
-			// Rewrite the 'onFocus' handler to always focus 'url' field.
-			dialogDefinition.onFocus = function()
-			{
-				var urlField = this.getContentElement( 'info', 'url' );
-				urlField.select();
-			};
-
-			*/
 		}
 
 		if ( dialogName == 'image' )
@@ -200,139 +165,10 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 		}
 	});
 
-	//]]>
 	</script>
 
 	<?php
 	
-	/*
-	?>
-		
-		
-		<script type="text/javascript" src="<?php echo $base_url; ?>/media/jscript/textbb/ckeditor/ckeditor.js"></script>
-		<script type="text/javascript">
-			//<![CDATA[
-
-			// Replace the <textarea id="editor"> with an CKEditor
-			// instance, using the "bbcode" plugin, shaping some of the
-			// editor configuration to fit BBCode environment.
-
-			CKEDITOR.replace( '<?php echo $name_editor; ?>',
-				{
-					extraPlugins : 'bbcode',
-					// Remove unused plugins.
-					removePlugins : 'bidi,button,dialogadvtab,div,filebrowser,flash,format,forms,horizontalrule,iframe,indent,justify,liststyle,pagebreak,showborders,stylescombo,table,tabletools,templates',
-					// Width and height are not supported in the BBCode format, so object resizing is disabled.
-					disableObjectResizing : true,
-					// Define font sizes in percent values.
-					fontSize_sizes : "30/30%;50/50%;100/100%;120/120%;150/150%;200/200%;300/300%",
-					
-					toolbar :
-					[
-						['Source'],'-', ['Bold', 'Italic','Underline'], '-', ['Blockquote'] , '-', ['Link', 'Unlink'], '-', ['TextColor', 'FontSize', 'SpecialChar'], '-', ['Image'], '-', ['Undo','Redo'], '-', ['Smiley'], 'NewPage','-','Undo','Redo'],
-						['Find','Replace','-','SelectAll','RemoveFormat'],
-						['Link', 'Unlink', 'Image', 'Smiley','SpecialChar'],
-						'/',
-						['Bold', 'Italic','Underline'],
-						['FontSize'],
-						['TextColor'],
-						['NumberedList','BulletedList','-','Blockquote'],
-						['Maximize']
-					],
-					// Strip CKEditor smileys to those commonly used in BBCode.
-					
-					smiley_path: ['<?php echo $base_url; ?>/media/smileys/'],
-					smiley_images :
-					[
-						
-						<?php
-						echo '\''.implode('\',\'', $smiley_img).'\'';
-
-						?>
-						'regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','tounge_smile.gif','embaressed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angel_smile.gif','shades_smile.gif', 'cry_smile.gif','kiss.gif'
-					],
-					
-					smiley_descriptions :
-					[
-						
-						<?php
-						echo '\''.implode('\',\'', $smiley_text).'\'';
-
-						?>
-						
-						'smiley', 'sad', 'wink', 'laugh', 'cheeky', 'blush', 'surprise',
-						'indecision', 'angel', 'cool', 'crying'
-					]
-					
-			} );
-
-			//]]>
-			</script>
-			
-	*/
-	
-	/*
-	?>
-	<script language="Javascript">
-	
-	textbb=document.getElementById('<?php echo $name_editor.'_iframe'; ?>');
-	framebb=document.getElementById('<?php echo $name_editor; ?>').contentWindow.document;
-
-	StartTextBB();
-
-	function StartTextBB() 
-	{
-
-		if(document.designMode)
-		{
-			alert(document.getElementById('<?php echo $name_editor; ?>').contentWindow.document.body.textContent);
-			for(pepe in document.getElementById('<?php echo $name_editor; ?>').contentWindow.document)
-			{
-
-				document.write('<p>'+pepe);
-
-			}
-			
-			framebb.designMode = "on";
-			framebb.write('');
-			framebb.close();
-			//framebb.execCommand('Bold', false, null);
-			//framebb.execCommand('inserthtml', false, 'pepe'); 
-			
-			first_code='<?php echo do_bbcode($value); ?>';
-			framebb.execCommand('inserthtml', false, first_code); 
-
-
-		}
-		else
-		{
-
-			textbb.innerHTML='<?php echo TextAreaForm($name_editor, '', $value); ?>';
-
-		}
-		
-	//}
-
-	function put_bold()
-	{
-
-		//alert(open_tag+' '+close_tag);
-		framebb.execCommand('Bold', false, null);
-
-	}
-
-	function emoticon(smiley_text, smiley_image)
-	{
-
-		//framebb.write(' <img src="'+smiley_image+'" alt="'+smiley_text+'"/> ');
-		imagesrc=' <img src="'+smiley_image+'" alt="'+smiley_text+'"/> ';
-		framebb.execCommand('inserthtml', false, imagesrc); 
-
-	}
-
-	</script>
-	<?php
-	*/
 }
 
 ?>
