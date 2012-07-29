a882ab83ead767cede78a7804944d699d9b0231c
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-07-29 03:54:32 +0200

    Little fix in links for blocks

--- modules/blocks/extra_links/user.php
+++ modules/blocks/extra_links/user.php
@@ -4,9 +4,13 @@ global $lang;
 
 load_lang('user');
 
+$select_page[]=$lang['user_admin']['user']; 
+$select_page[]='optgroup';
+
 $select_page[]=$lang['user_admin']['user'];
 $select_page[]=make_fancy_url($base_url, 'user', 'index', 'user_zone', $arr_data=array());
 
-
+$select_page[]=''; 
+$select_page[]='end_optgroup';
 
 ?>
