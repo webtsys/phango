d0a7089171efd84254ae752961146d1ed291915a
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-10-08 03:18:20 +0200

    Fixes in banners

---
+++ modules/blocks/html/banners.php
@@ -0,0 +1,15 @@
+<?php
+
+global $model;
+
+$query_block=$model['banners']->select('where position_banner="blocks'.$result['IdBlocks'].'"', array('IdBanners', 'content') );
+
+while(list($idbanner, $content_banner)=webtsys_fetch_row($query_block))
+{
+
+	echo '<p>'.$content_banner.'<p>';
+
+}
+
+
+?>
