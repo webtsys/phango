0a60fdd44c4db047a473ea23edfe0e2edd0bdf81
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-11-20 04:03:41 +0100

    Fix bug #6

--- libraries/search_in_field.php
+++ libraries/search_in_field.php
@@ -32,7 +32,7 @@ function search_in_field($model_name, $fields, $phrase_search)
 		foreach($fields as $field)
 		{
 
-			$arr_loc[]="IF(LOCATE(\"$phrase_search\",".$model_name.".".$field."),2,0)";
+			$arr_loc[]="IF(LOCATE(\"$phrase_search\",".$model_name.".`".$field."`),2,0)";
 
 		}
 
@@ -46,8 +46,8 @@ function search_in_field($model_name, $fields, $phrase_search)
 				foreach($fields as $field)
 				{
 
-					$arr_final[]=" ".$model_name.".".$field." LIKE \"%$value%\""; 
-					$arr_loc[]="IF(LOCATE(\"$value\",".$model_name.".".$field."),1,0)";
+					$arr_final[]=" ".$model_name.".`".$field."` LIKE \"%$value%\""; 
+					$arr_loc[]="IF(LOCATE(\"$value\",".$model_name.".`".$field."`),1,0)";
 
 				}
 
@@ -63,17 +63,17 @@ function search_in_field($model_name, $fields, $phrase_search)
 
 		$final_text=implode(" or ",$arr_final);
 		
-		if(isset($model[$model_name]->components[$field]->related_model))
+		/*if(isset($model[$model_name]->components[$field]->related_model))
 		{
 
 			
 
-		}
+		}*/
 
 		foreach($fields as $field)
 		{
 
-			$sql_text[]="( ".$model_name.".".$field." like \"%$phrase_search%\" or ".$model_name.".".$field." like \"$phrase_search%\" or  ".$model_name.".".$field." like \"%$phrase_search\"";
+			$sql_text[]="( ".$model_name.".`".$field."` like \"%$phrase_search%\" or ".$model_name.".`".$field."` like \"$phrase_search%\" or  ".$model_name.".`".$field."` like \"%$phrase_search\"";
 
 		}
 
