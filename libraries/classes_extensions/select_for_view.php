67ce0bfbbb5409bf6757711687bd02d042dc1b58
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-12-17 05:49:13 +0100

    Added fixes to select_for_view

--- libraries/classes_extensions/select_for_view.php
+++ libraries/classes_extensions/select_for_view.php
@@ -1,11 +1,15 @@
 <?php
 
-function select_for_view_method_class($class, $views, $conditions="", $arr_select=array(), $raw_query=0)
+function select_for_view_method_class($class, $param_templates, $conditions="", $arr_select=array(), $raw_query=0)
 {
 
 	//Load view...
 	
+	load_libraries_views($param_templates['view_library'], $param_templates['func_views']);
+	
 	//Load header...
+	
+	echo load_view($param_templates['func_views']['header']['arr_template'], $param_templates['func_views']['header']['params_template']);
 	
 	//Load query
 
@@ -13,7 +17,16 @@ function select_for_view_method_class($class, $views, $conditions="", $arr_selec
 	
 	//Load interval...
 	
+	while($arr_element=webtsys_fetch_row($query))
+	{
+	
+		echo load_view($arr_element, $param_templates['func_views'][1]['params_template']);
+	
+	}
+	
 	//Load footer
+	
+	echo load_view($param_templates['func_views']['header']['footer'], $param_templates['func_views']['footer']['params_template']);
 
 }
 
