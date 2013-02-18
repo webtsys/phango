430fffbc060d24c69b3264c3f34404a7320ef457
Antonio de la Rosa <webmaster@web-t-sys.com>
2013-02-18 01:05:55 +0100

    Added select_a_row.hp

---
+++ libraries/classes_extensions/select_a_row.php
@@ -0,0 +1,14 @@
+<?php
+
+function select_a_row_method_class($class, $idrow, $arr_select=array(), $raw_query=0, $assoc=0)
+{
+
+	settype($idrow, 'integer');
+	
+	$query=$class->select('where '.$class->idmodel.'=\''.$idrow.'\'', $arr_select, $raw_query);
+	
+	return webtsys_fetch_array($query, $assoc);
+
+}
+
+?>
