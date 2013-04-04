10d2eeeb45cdb0d874fae7fac25c04d5e2b874f5
Antonio de la Rosa <webmaster@web-t-sys.com>
2013-04-05 01:24:11 +0200

    Added fix on element_exists extension

--- libraries/classes_extensions/element_exists.php
+++ libraries/classes_extensions/element_exists.php
@@ -1,11 +1,18 @@
 <?php
 
-function element_exists_class($class, $idrow)
+function element_exists_class($class, $idrow, $field_search='')
 {
+
+	if($field_search=='')
+	{
+	
+		$field_search=$class->idmodel;
+	
+	}
 
 	settype($idrow, 'integer');
 	
-	$num_elements=$class->select_count('where '.$class->idmodel.'=\''.$idrow.'\'', $class->idmodel);
+	$num_elements=$class->select_count('where '.$field_search.'=\''.$idrow.'\'', $class->idmodel);
 	
 	return $num_elements;
 
