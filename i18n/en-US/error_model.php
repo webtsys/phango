630178e45e0a76e73df0c254ec93ebadd2a97749
Antonio de la Rosa <webmaster@web-t-sys.com>
2013-04-06 01:54:16 +0200

    Modifications on i18n files

--- i18n/en-US/error_model.php
+++ i18n/en-US/error_model.php
@@ -1,7 +1,5 @@
 <?php
 
-$lang['error_model']['email_format_error']='Error in email format';
-
 $lang['error_model']['cant_insert']='I can not insert the data';
 
 $lang['error_model']['cant_update']='I can not update the data';
@@ -11,6 +9,8 @@ $lang['error_model']['check_error_field']='Error in field';
 $lang['error_model']['check_error_field_required']='Error: field required';
 
 $lang['error_model']['img_format_error']='Error in image format';
+
+$lang['error_model']['email_format_error']='Error in email format';
 
 $lang['error_model']['file_no_exists']='File don&amp;#39;t found';
 
