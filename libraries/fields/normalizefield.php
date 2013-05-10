4072d984ae63c45dcc5de6fa6495b6512a57b288
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2013-05-10 18:28:24 +0200

    Fixes on normalize

--- libraries/fields/normalizefield.php
+++ libraries/fields/normalizefield.php
@@ -8,6 +8,15 @@ class NormalizeField extends TextField {
 	public function check($value)
 	{
 		
+		
+		return $this->check_text($value);
+		
+
+	}
+	
+	static public function check_text($value)
+	{
+	
 		$str_normalize=slugify(strip_tags($value));
 		
 		$arr_normalize=explode('-', $str_normalize);
@@ -17,10 +26,8 @@ class NormalizeField extends TextField {
 		$value=implode('%', $arr_normalize);
 		
 		return $value;
-		
-
-	}
 	
+	}
 
 }
 
