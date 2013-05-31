91c3a7b9244c9ac4bc017f83f94cb6d2e092a9af
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2013-05-31 18:07:16 +0200

    Little modification on wrapwords

--- libraries/utilities/wrap_words.php
+++ libraries/utilities/wrap_words.php
@@ -1,6 +1,6 @@
 <?php
 
-function wrap_words($text, $num_words, $text_explain='...')
+function wrap_words($text, $num_words, $text_explain='...', $yes_more_ever=0)
 {
 
 	$arr_text=explode(' ', $text);
@@ -13,7 +13,11 @@ function wrap_words($text, $num_words, $text_explain='...')
 	{
 
 		$num_words=$total_num_text;
-		$text_explain='';
+		
+		if($yes_more_ever==0)
+		{
+			$text_explain='';
+		}
 
 	}
 
