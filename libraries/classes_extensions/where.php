7925319d0104ceff2fd78b69525823bde0368849
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2013-07-23 14:11:40 +0200

    More fixes on new extension for the orm

--- libraries/classes_extensions/where.php
+++ libraries/classes_extensions/where.php
@@ -6,20 +6,24 @@
 *
 */
 
-function where_method_class($class, $arr_where)
+function where_method_class($class, $arr_where, $initial_sql='WHERE')
 {
 	
 	foreach($arr_where as $type => $where)
 	{
 	
-		//Checking
+		/*//Checking
 	
 		foreach($where as $field => $value)
 		{
 		
 			$where[$field]=$class->components[$field]->check($value);
 		
-		}
+		}*/
+		
+		$arr_sql=array();
+		
+		$sql_string='';
 	
 		switch($type)
 		{
@@ -27,49 +31,91 @@ function where_method_class($class, $arr_where)
 			//Default is and
 			default:
 			
+				foreach($where as $field => $value)
+				{
 				
+					$arr_sql[]='`'.$class->name.'`'.'.'.'`'.$field.'`'.'=\''.$class->components[$field]->check($value).'\'';
+				
+				}
+				
+				$initial_sql.=' '.implode(' AND ', $arr_sql);
 			
 			break;
 			
 			case 'OR':
 			
+				foreach($where as $field => $value)
+				{
 				
+					$arr_sql[]='`'.$class->name.'`'.'.'.'`'.$field.'`'.'=\''.$class->components[$field]->check($value).'\'';
+				
+				}
+				
+				$initial_sql.=' '.implode(' OR ', $arr_sql);
 			
 			break;
 			
 			case 'IN':
 			
+				/*foreach($where as $field => $value)
+				{
+					
+					$where[$field]=$class->components[$field]->check($value);
+				
+				}
+				
+				$initial_sql.=' '.'`'.$class->name.'`'.'.'.'`'.$field.'`'.' IN (\''.implode('\', ', $where).'\')';*/
+				
 				
 			
 			break;
 			
 			case 'NOT IN':
 			
+				foreach($where as $field => $value)
+				{
 				
+					//$arr_sql[]='`'.$class->name.'`'.'.'.'`'.$field.'`'.' IN \''.$class->components[$field]->check($value).'\'';
+					
+					$where[$field]=$class->components[$field]->check($value);
+				
+				}
+				
+				$initial_sql.=' '.'`'.$class->name.'`'.'.'.'`'.$field.'`'.' NOT IN (\''.implode('\', ', $where).'\')';
 			
 			break;
 			
-			case 'LIKE':
+			case 'LIKE_OR':
 			
+				foreach($where as $field => $value)
+				{
 				
+					$arr_sql[]=' '.'`'.$class->name.'`'.'.'.'`'.$field.'`'.' LIKE \'%'.$class->components[$field]->check($value).'%\'';
+				
+				}
+				
+				$initial_sql.=' '.implode(' OR ', $arr_sql);
 			
 			break;
 			
-			case 'GROUP BY':
+			case 'LIKE_AND':
 			
+				foreach($where as $field => $value)
+				{
 				
-			
-			break;
-			
-			case 'ORDER BY':
-			
+					$arr_sql[]='`'.$class->name.'`'.'.'.'`'.$field.'`'.' LIKE \'%'.$class->components[$field]->check($value).'%\'';
 				
+				}
+				
+				$initial_sql.=' '.implode(' AND ', $arr_sql);
 			
 			break;
 		
 		}
 	
 	}
+	
+	return $initial_sql;
 	
 }
 
