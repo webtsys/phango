83b54fd781728e1438968845b66e8adfbc2b0d8f
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2014-01-20 12:21:30 +0100

    Fixes on generate_admin

--- libraries/admin/generate_admin_class.php
+++ libraries/admin/generate_admin_class.php
@@ -1,5 +1,7 @@
 <?php
 
+load_libraries(array('utilities/menu_barr_hierarchy', 'generate_admin_ng'));
+
 class GenerateAdminClass {
 
 	public $class, $arr_fields, $arr_fields_edit, $url_options, $options_func, $where_sql, $arr_fields_form, $type_list, $url_back, $no_search, $txt_list_new, $txt_add_new_item, $txt_edit_item, $simple_redirect, $class_add, $separator_element_opt, $extra_menu_create;
@@ -53,8 +55,6 @@ class GenerateAdminClass {
 		settype($_GET['op_edit'], 'integer');
 		settype($_GET['op_action'], 'integer');
 		settype($_GET[$model[$this->model_name]->idmodel], 'integer');
-		
-		load_libraries(array('generate_admin_ng', 'utilities/menu_barr_hierarchy'));
 
 		$url_admin=add_extra_fancy_url($this->url_options, array('op_action' => 1));
 		
@@ -278,182 +278,5 @@ class ListModelClass {
 
 }
 
-//Class for search, the function is deprecated when need many arguments, is more easy use classes for its.
-
-class SearchInFieldClass {
-
-	public $model_name, $arr_fields_order, $arr_fields_search, $where_sql, $url_options, $yes_id=1, $show_form=1, $lang_asc;
-
-	function __construct($model_name, $arr_fields_order, $arr_fields_search, $where_sql, $url_options, $yes_id=1, $show_form=1)
-	{
-	
-		global $lang;
-	
-		$this->model_name=$model_name;
-		$this->arr_fields_order=$arr_fields_order;
-		$this->arr_fields_search=$arr_fields_search;
-		$this->where_sql=$where_sql;
-		$this->url_options=$url_options;
-		$this->yes_id=$yes_id;
-		$this->show_form=$show_form;
-		$this->lang_asc=$lang['common']['ascent'];
-		$this->lang_desc=$lang['common']['descent'];
-	
-	
-	}
-	
-	public function search()
-	{
-	
-		global $lang, $model;
-	
-		load_libraries(array('search_in_field'));
-
-		if(count($model[$this->model_name]->forms)==0)
-		{
-
-			$arr_error_sql[0]='Do you need create a form for this model';    
-			$arr_error_sql[1]='Do you need create a form for this model '.$this->model_name.' for use SearchInField function';
-			ob_end_clean();
-			echo load_view(array('title' => 'Phango site is down', 'content' => '<p>'.$arr_error_sql[DEBUG].'</p>'), 'common/common');
-			die();
-
-		}
-
-		if(!in_array($model[$this->model_name]->idmodel, $this->arr_fields_order) && $this->yes_id==1)
-		{
-
-			array_unshift($this->arr_fields_order, $model[$this->model_name]->idmodel);
-			array_unshift($this->arr_fields_search, $model[$this->model_name]->idmodel);
-			
-			$model[$this->model_name]->forms[ $model[$this->model_name]->idmodel ]->label='#Id.';
-
-		}
-
-		//Set order
-
-		$_GET['order_field']=@form_text($_GET['order_field']);
-		$_GET['search_word']=@form_text($_GET['search_word']);
-		$_GET['search_field']=@form_text($_GET['search_field']);
-		
-		$arr_order_select=array();
-
-		if( !in_array($_GET['order_field'], $this->arr_fields_order) )
-		{
-
-			$_GET['order_field']=$this->arr_fields_order[0]; //$model[$model_name]->idmodel;
-
-		}
-		
-		if( !in_array($_GET['search_field'], $this->arr_fields_search) )
-		{
-
-			$_GET['search_field']=$this->arr_fields_search[0]; //$model[$model_name]->idmodel;
-
-		}
-		
-		//0=DESC
-		//1=ASC
-
-		settype($_GET['order_desc'], 'integer');
-			
-		$arr_order[$_GET['order_desc']]='ASC';
-
-		$arr_order[0]='ASC';
-		$arr_order[1]='DESC';
-
-		$arr_order_select[]=$_GET['order_desc'];
-		$arr_order_select[]=$this->lang_asc;
-		$arr_order_select[]=0;
-		$arr_order_select[]=$this->lang_desc;
-		$arr_order_select[]=1;
-
-		$arr_order_field=array($_GET['order_field']);
-		$arr_search_field=array($_GET['search_field']);
-
-		foreach($this->arr_fields_order as $field_label)
-		{
-
-			$arr_order_field[]=$model[$this->model_name]->forms[$field_label]->label;
-			$arr_order_field[]=$field_label;
-
-		}
-		
-		foreach($this->arr_fields_search as $field_label)
-		{
-
-			$arr_search_field[]=$model[$this->model_name]->forms[$field_label]->label;
-			$arr_search_field[]=$field_label;
-
-		}
-		
-		if($this->show_form==1)
-		{
-			echo load_view(array($arr_search_field, $arr_order_field, $arr_order_select, $this->url_options), 'common/forms/searchform');
-		}
-		//Query for order
-
-		//Query for search_by
-		
-		/*list($location, $arr_where_sql)=search_in_field($model_name, array($_GET['search_field']), $_GET['search_word']);
-		
-		if($location!='')
-		{
-
-			$location=$location.' DESC ,';
-
-		}*/
-		
-		$location='';
-		
-		$arr_where_sql='';
-		
-		if(isset($model[$this->model_name]->components[$_GET['search_field']]) && $_GET['search_word']!='')
-		{
-		
-			$value_search=$model[$this->model_name]->components[$_GET['search_field']]->search_field($_GET['search_word']);
-			
-			if(get_class($model[$this->model_name]->components[$_GET['search_field']])!='ForeignKeyField')
-			{
-			
-				$arr_where_sql='`'.$this->model_name.'`.`'.$_GET['search_field'].'` LIKE \'%'.$value_search.'%\'';
-				
-			}
-			else
-			{
-			
-				$model_related_name=$model[$this->model_name]->components[$_GET['search_field']]->related_model;
-				
-				if($model[$this->model_name]->components[$_GET['search_field']]->name_field_to_field!='')
-				{
-				
-					$field_related_name=$model[$this->model_name]->components[$_GET['search_field']]->name_field_to_field;
-					
-					$arr_where_sql='`'.$model_related_name.'`.`'.$field_related_name.'` LIKE \'%'.$value_search.'%\'';
-					
-				}
-			
-			}
-		
-		}
-
-		if($this->where_sql=='' && $arr_where_sql!='')
-		{
-			
-			$this->where_sql='where ';
-
-		}
-		else if($this->where_sql!='' && $arr_where_sql!='')
-		{
-
-			$this->where_sql.=' AND ';
-
-		}
-		
-		return array($this->where_sql, $arr_where_sql, $location, $arr_order);
-	
-	}
-
-}
 
 ?>
--- libraries/generate_admin_ng.php
+++ libraries/generate_admin_ng.php
@@ -1,6 +1,6 @@
 <?php
 
-load_libraries(array('generate_forms', 'table_config', 'pages', 'admin/generate_admin_class'));
+load_libraries(array('generate_forms', 'table_config', 'pages'));
 
 function InsertModelForm($model_name, $url_admin, $url_back, $arr_fields=array(), $id=0, $goback=1, $simple_redirect=0)
 {
@@ -494,6 +494,10 @@ function BasicList($model_name, $where_sql, $arr_where_sql, $location, $arr_orde
 	//Quit id if don't need
 
 	$arr_strip_fields=$arr_fields;
+	
+	$remove=new remove_idrow();
+	
+	$remove_method='';
 
 	if($yes_id==1)
 	{
@@ -501,12 +505,16 @@ function BasicList($model_name, $where_sql, $arr_where_sql, $location, $arr_orde
 		$model[$model_name]->forms[ $model[$model_name]->idmodel ]->label='#Id.';
 		$cell_sizes[$model[$model_name]->idmodel]=' width="18" align="center"';
 
-		function remove_idrow($arr_row, $model_idmodel)
+		/*function remove_idrow($arr_row, $model_idmodel)
 		{
 
 			return $arr_row;
 
-		}
+		}*/
+		
+		//$remove->no_remove($arr_row, $model_idmodel);
+		
+		$remove_method='no_remove';
 
 	}
 	else
@@ -514,42 +522,54 @@ function BasicList($model_name, $where_sql, $arr_where_sql, $location, $arr_orde
 
 		unset($arr_label_fields[$model[$model_name]->idmodel]);
 
-		function remove_idrow($arr_row, $model_idmodel)
+		/*function remove_idrow($arr_row, $model_idmodel)
 		{
 
 			unset($arr_row[$model_idmodel]);	
 
 			return $arr_row;
 
-		}
+		}*/
+		
+		//$remove->remove();
+		
+		$remove_method='remove';
 
 	}
+	
+	$add_options=new add_options();
+	
+	$add_options_method='';
 	
 	if($yes_options==1)
 	{
 
 		$arr_label_fields[]=$lang['common']['options'];
 
-		function add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
+		/*function add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
 		{
 
 			$arr_row[]=implode($separator_element, $options_func($url_options, $model_name, $model_idmodel, $arr_row_raw) );
 
 			return $arr_row;
 
-		}
+		}*/
+		
+		$add_options_method='yes_add_options';
 
 	}
 	else
 	{
 
 
-		function add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
+		/*function add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
 		{
 
 			return $arr_row;
 
-		}
+		}*/
+		
+		$add_options_method='no_add_options';
 
 	}
 	
@@ -585,9 +605,9 @@ function BasicList($model_name, $where_sql, $arr_where_sql, $location, $arr_orde
 
 		}
 		
-		$arr_row=add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $arr_row[$model[$model_name]->idmodel], $separator_element);
+		$arr_row=$add_options->$add_options_method($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $arr_row[$model[$model_name]->idmodel], $separator_element);
 
-		$arr_row=remove_idrow($arr_row, $model[$model_name]->idmodel);
+		$arr_row=$remove->$remove_method($arr_row, $model[$model_name]->idmodel);
 
 		middle_table_config($arr_row, $cell_sizes);
 
@@ -844,6 +864,226 @@ function GeneratePositionModel($model_name, $field_name, $field_position, $url, 
 	{
 
 		echo '<p>'.$lang['common']['no_exists_elements_to_order'].'</p>';
+
+	}
+
+}
+
+//Class for search, the function is deprecated when need many arguments, is more easy use classes for its.
+
+class SearchInFieldClass {
+
+	public $model_name, $arr_fields_order, $arr_fields_search, $where_sql, $url_options, $yes_id=1, $show_form=1, $lang_asc;
+
+	function __construct($model_name, $arr_fields_order, $arr_fields_search, $where_sql, $url_options, $yes_id=1, $show_form=1)
+	{
+	
+		global $lang;
+	
+		$this->model_name=$model_name;
+		$this->arr_fields_order=$arr_fields_order;
+		$this->arr_fields_search=$arr_fields_search;
+		$this->where_sql=$where_sql;
+		$this->url_options=$url_options;
+		$this->yes_id=$yes_id;
+		$this->show_form=$show_form;
+		$this->lang_asc=$lang['common']['ascent'];
+		$this->lang_desc=$lang['common']['descent'];
+	
+	
+	}
+	
+	public function search()
+	{
+	
+		global $lang, $model;
+	
+		load_libraries(array('search_in_field'));
+
+		if(count($model[$this->model_name]->forms)==0)
+		{
+
+			$arr_error_sql[0]='Do you need create a form for this model';    
+			$arr_error_sql[1]='Do you need create a form for this model '.$this->model_name.' for use SearchInField function';
+			ob_end_clean();
+			echo load_view(array('title' => 'Phango site is down', 'content' => '<p>'.$arr_error_sql[DEBUG].'</p>'), 'common/common');
+			die();
+
+		}
+
+		if(!in_array($model[$this->model_name]->idmodel, $this->arr_fields_order) && $this->yes_id==1)
+		{
+
+			array_unshift($this->arr_fields_order, $model[$this->model_name]->idmodel);
+			array_unshift($this->arr_fields_search, $model[$this->model_name]->idmodel);
+			
+			$model[$this->model_name]->forms[ $model[$this->model_name]->idmodel ]->label='#Id.';
+
+		}
+
+		//Set order
+
+		$_GET['order_field']=@form_text($_GET['order_field']);
+		$_GET['search_word']=@form_text($_GET['search_word']);
+		$_GET['search_field']=@form_text($_GET['search_field']);
+		
+		$arr_order_select=array();
+
+		if( !in_array($_GET['order_field'], $this->arr_fields_order) )
+		{
+
+			$_GET['order_field']=$this->arr_fields_order[0]; //$model[$model_name]->idmodel;
+
+		}
+		
+		if( !in_array($_GET['search_field'], $this->arr_fields_search) )
+		{
+
+			$_GET['search_field']=$this->arr_fields_search[0]; //$model[$model_name]->idmodel;
+
+		}
+		
+		//0=DESC
+		//1=ASC
+
+		settype($_GET['order_desc'], 'integer');
+			
+		$arr_order[$_GET['order_desc']]='ASC';
+
+		$arr_order[0]='ASC';
+		$arr_order[1]='DESC';
+
+		$arr_order_select[]=$_GET['order_desc'];
+		$arr_order_select[]=$this->lang_asc;
+		$arr_order_select[]=0;
+		$arr_order_select[]=$this->lang_desc;
+		$arr_order_select[]=1;
+
+		$arr_order_field=array($_GET['order_field']);
+		$arr_search_field=array($_GET['search_field']);
+
+		foreach($this->arr_fields_order as $field_label)
+		{
+
+			$arr_order_field[]=$model[$this->model_name]->forms[$field_label]->label;
+			$arr_order_field[]=$field_label;
+
+		}
+		
+		foreach($this->arr_fields_search as $field_label)
+		{
+
+			$arr_search_field[]=$model[$this->model_name]->forms[$field_label]->label;
+			$arr_search_field[]=$field_label;
+
+		}
+		
+		if($this->show_form==1)
+		{
+			echo load_view(array($arr_search_field, $arr_order_field, $arr_order_select, $this->url_options), 'common/forms/searchform');
+		}
+		//Query for order
+
+		//Query for search_by
+		
+		/*list($location, $arr_where_sql)=search_in_field($model_name, array($_GET['search_field']), $_GET['search_word']);
+		
+		if($location!='')
+		{
+
+			$location=$location.' DESC ,';
+
+		}*/
+		
+		$location='';
+		
+		$arr_where_sql='';
+		
+		if(isset($model[$this->model_name]->components[$_GET['search_field']]) && $_GET['search_word']!='')
+		{
+		
+			$value_search=$model[$this->model_name]->components[$_GET['search_field']]->search_field($_GET['search_word']);
+			
+			if(get_class($model[$this->model_name]->components[$_GET['search_field']])!='ForeignKeyField')
+			{
+			
+				$arr_where_sql='`'.$this->model_name.'`.`'.$_GET['search_field'].'` LIKE \'%'.$value_search.'%\'';
+				
+			}
+			else
+			{
+			
+				$model_related_name=$model[$this->model_name]->components[$_GET['search_field']]->related_model;
+				
+				if($model[$this->model_name]->components[$_GET['search_field']]->name_field_to_field!='')
+				{
+				
+					$field_related_name=$model[$this->model_name]->components[$_GET['search_field']]->name_field_to_field;
+					
+					$arr_where_sql='`'.$model_related_name.'`.`'.$field_related_name.'` LIKE \'%'.$value_search.'%\'';
+					
+				}
+			
+			}
+		
+		}
+
+		if($this->where_sql=='' && $arr_where_sql!='')
+		{
+			
+			$this->where_sql='where ';
+
+		}
+		else if($this->where_sql!='' && $arr_where_sql!='')
+		{
+
+			$this->where_sql.=' AND ';
+
+		}
+		
+		return array($this->where_sql, $arr_where_sql, $location, $arr_order);
+	
+	}
+
+}
+
+class remove_idrow {
+
+	function no_remove($arr_row, $model_idmodel)
+	{
+
+		return $arr_row;
+
+	}
+
+	function remove($arr_row, $model_idmodel)
+	{
+
+		unset($arr_row[$model_idmodel]);	
+
+		return $arr_row;
+
+	}
+
+}
+
+class add_options {
+
+	function yes_add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
+	{
+
+		$arr_row[]=implode($separator_element, $options_func($url_options, $model_name, $model_idmodel, $arr_row_raw) );
+
+		return $arr_row;
+
+	}
+
+
+
+	function no_add_options($arr_row, $arr_row_raw, $options_func, $url_options, $model_name, $model_idmodel, $separator_element)
+	{
+
+		return $arr_row;
 
 	}
 
