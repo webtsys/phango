842cf010436211247d9002695830f6c7cc211c97
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2014-01-24 19:38:20 +0100

    Added showmedia

---
+++ modules/media/controllers/controller_showmedia.php
@@ -0,0 +1,70 @@
+<?php
+
+function ShowMedia()
+{
+
+	global $base_path, $config_data;
+
+	settype($_GET['images'], 'string');
+	settype($_GET['css'], 'string');
+	
+	$_GET['images']=basename(form_text($_GET['images']));
+	$_GET['css']=basename(form_text($_GET['css']));
+	
+	//Accept .gif, .png o .jpg
+	
+	if($_GET['images']!='')
+	{
+	
+		$ext_info=pathinfo($_GET['images']);
+		
+		if($ext_info['extension']=='gif' || $ext_info['extension']=='jpg' || $ext_info['extension']=='png')
+		{
+		
+			$file_path=$base_path.$config_data['module_theme'].'/media/images/'.$_GET['images'];
+			
+			if(file_exists($file_path))
+			{
+			
+				header('Content-Type: image/'.$ext_info['extension']);
+			
+				readfile($file_path);
+			
+			}
+			
+		}
+		
+		die;
+	
+	}
+	
+	if($_GET['css']!='')
+	{
+		
+		$ext_info=pathinfo($_GET['css']);
+		
+		if($ext_info['extension']=='css')
+		{
+		
+			$file_path=$base_path.$config_data['module_theme'].'/media/css/'.$_GET['css'];
+			
+			if(file_exists($file_path))
+			{
+			
+				header('Content-Type: text/css');
+			
+				readfile($file_path);
+			
+			}
+			
+		}
+		
+		die;
+	
+	}
+	
+	
+
+}
+
+?>
