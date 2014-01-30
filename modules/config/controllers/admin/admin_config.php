d2bd90e6aeadd4dcafe31999d3364c4e9fb2627c
webmaster@web-t-sys.com <webmaster@web-t-sys.com>
2014-01-30 18:51:31 +0100

    Fixes on principal config

--- modules/config/controllers/admin/admin_config.php
+++ modules/config/controllers/admin/admin_config.php
@@ -31,7 +31,7 @@ function ConfigAdmin()
 
 	while ( $appearance = readdir( $dir ) )
 	{
-		if ( $appearance != "." && $appearance != ".." && !preg_match('/^\./', $appearance) )
+		if ( $appearance != "." && $appearance != ".." && !preg_match('/^\./', $appearance) && is_dir($base_path."views/".$appearance))
 		{
 
 			if( file_exists($base_path."views/".$appearance."/.info_theme") )
@@ -53,7 +53,7 @@ function ConfigAdmin()
 	
 	while ( $appearance = readdir( $dir ) )
 	{
-		if ( $appearance != "." && $appearance != ".." && !preg_match('/^\./', $appearance) )
+		if ( $appearance != "." && $appearance != ".." && !preg_match('/^\./', $appearance) && is_dir($base_path."views/".$appearance))
 		{
 			
 			if( file_exists($base_path."modules/".$appearance."/views/".$appearance."/.info_theme") )
