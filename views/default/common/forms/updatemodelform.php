e0016c8a1b2894757b8f120216188989c1f12020
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-09-28 20:19:01 +0200

    Adding support for tabs on modelform view

--- views/default/common/forms/updatemodelform.php
+++ views/default/common/forms/updatemodelform.php
@@ -1,19 +1,88 @@
 <?php
 
-function UpdateModelFormView($model_form, $arr_fields=array(), $url_post, $enctype='', $form_html_id='', $arr_categories=array('default'))
+function UpdateModelFormView($model_form, $arr_fields=array(), $url_post, $enctype='', $form_html_id='', $arr_categories=array('default' => array()))
 {
 
-	global $lang;
+	global $lang, $arr_cache_jscript, $arr_cache_header;
 
 	load_libraries(array('generate_forms'));
+	
+	$arr_cache_jscript[]='jquery.min.js';
+	
+	$hide_button_tab=1;
+	
+	if(isset($arr_categories['default']))
+	{
+	
+		$arr_categories['default']=&$arr_fields;
+	
+		$hide_button_tab=1;
+	
+	}
 
+	ob_start();
+	?>
+	<script language="javascript">
+	
+	$(document).ready( function () {
+	
+		<?php
+		
+		if($hide_button_tab==1)
+		{
+		
+		?>
+		
+		$('.form_button_tab').hide();
+		
+		<?php
+		
+		}
+		else
+		{
+		
+		?>
+	
+		$('.form_button_tab').click( function () {
+		
+			
+		
+		});
+		
+		<?php
+		
+		}
+		
+		?>
+	
+	});
+	
+	</script>
+	<?php
+	
+	$arr_cache_header[]=ob_get_contents();
+
+	ob_end_clean();
+	
 	?>
 	<form method="post" action="<?php echo $url_post; ?>" name="form" id="form<?php echo $form_html_id; ?>" <?php echo $enctype; ?>>
 	<?php
 	set_csrf_key();
 	
-	echo load_view(array($model_form, $arr_fields), 'common/forms/modelform');
-
+	foreach($arr_categories as $category => $arr_fields_tab)
+	{
+	
+	
+		?>
+		<div id="<?php echo $category; ?>_tag" class="form_tab">
+		<?php
+		
+		echo load_view(array($model_form, $arr_fields_tab), 'common/forms/modelform');
+	
+		?>
+		</div>
+		<?php
+	}
 	?>
 	<?php //echo set_csrf_key(); ?>
 	<input type="submit" value="<?php echo $lang['common']['send']; ?>" />
